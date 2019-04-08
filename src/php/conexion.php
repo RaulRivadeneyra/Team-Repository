2da426031af13f6dd494d825e889783b2e9ee2ed
Carlos Chan Gongora <43481488+Shadic78@users.noreply.github.com>
2019-04-08 14:23:38 -0500

    Login and register

---
+++ src/php/conexion.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  Codigo para conectarse a la base de datos
+*/
+
+	$servidor = "127.0.0.1";
+	$usuario = "root";
+	$pass = "";
+	$base = "AIDCA";
+
+	// Crear la conexion
+	$conexion = mysqli_connect($servidor,$usuario,$pass) or die ("No se conecto con el servidor");
+	$db = mysqli_select_db($conexion,$base) or die ("No se encontro la BD");
+
+	// Checar la conexion
+	if(mysqli_connect_errno()){
+		echo 'Conexion Fallida : ', mysqli_connect_error();
+		exit();
+	}
+?>
